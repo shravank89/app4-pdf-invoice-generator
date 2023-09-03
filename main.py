2439c4d193b2c1fd3bd66ee268e0b4beeb305205
Shravan <newshravank25@gmail.com>
2023-09-03 19:53:35 +0530

    adding data from excel using pandas

--- main.py
+++ main.py
@@ -6,18 +6,40 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath,sheet_name="Sheet 1")
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+    pdf = FPDF(orientation="L", unit="mm", format="A4")
     pdf.add_page()
 
     # Extracting date and filename from filepath using pathlib
     filename = Path(filepath).stem
-    path_info_list = filename.split("-")
-    invoice_nr = path_info_list[0]
-    date = path_info_list[1]
+    invoice_nr, date = filename.split("-")
 
     # Writing to PDF
     pdf.set_font(family="Courier", size=15, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice no. {invoice_nr}")
-    pdf.output(f"PDFs/{filename}.pdf")
+    pdf.cell(w=50, h=8, txt=f"Invoice no. {invoice_nr}", ln=1)
 
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Iterate over pandas df.columns object and making header
+    columns = [x.replace("_", " ").title() for x in df.columns]
+    pdf.set_font(family="Courier", size=10, style="B")
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=80, h=8, txt=columns[1], border=1)
+    pdf.cell(w=50, h=8, txt=columns[2], border=1)
+    pdf.cell(w=40, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # Adding data from pandas
+    for index, row in df.iterrows():
+        pdf.set_font(family="Courier", size=10)
+        pdf.set_text_color(100,100,100)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=80, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1,ln=1)
+
+    pdf.output(f"PDFs/{filename}.pdf")
